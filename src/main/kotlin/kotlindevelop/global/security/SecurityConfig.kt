d18f9b72b7eb3fbed98c2a21427ef3cd90dd160a
seosang-won <zmdk1205@naver.com>
2024-10-29 16:18:07 +0900

    feat : 시큐리티 필터 구현

---
+++ src/main/kotlin/kotlindevelop/global/security/SecurityConfig.kt
@@ -0,0 +1,54 @@
+package kotlindevelop.global.security
+
+import org.hibernate.query.sqm.tree.SqmNode.log
+import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+
+@Configuration
+@EnableMethodSecurity
+class SecurityConfig {
+
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+
+        http
+            .authorizeHttpRequests { authorize ->
+                authorize
+                    .requestMatchers(
+                        PathRequest.toStaticResources().atCommonLocations(),
+                        AntPathRequestMatcher("/resources/**"),
+                        AntPathRequestMatcher("/h2-console/**")
+                    ).permitAll()
+//                    .requestMatchers("/adm/**").hasRole("ADMIN")
+                    .anyRequest().permitAll()
+            }
+            .headers { headers ->
+                headers
+                    .addHeaderWriter(
+                        XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)
+                    )
+            }
+            .csrf { csrf ->
+                csrf.ignoringRequestMatchers("/h2-console/**")
+            }
+            .formLogin(Customizer.withDefaults())
+
+        return http.build()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+}
