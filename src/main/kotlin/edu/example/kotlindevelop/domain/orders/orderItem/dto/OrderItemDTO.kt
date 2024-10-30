3003b09cf9c06b5bb0cbea2e704bc331fdec08df
CryingPerson <opnice12@naver.com>
2024-10-30 10:42:48 +0900

    [mod] OrderItem 수정

--- src/main/kotlin/edu/example/kotlindevelop/domain/orders/orderItem/dto/OrderItemDTO.kt
+++ src/main/kotlin/edu/example/kotlindevelop/domain/orders/orderItem/dto/OrderItemDTO.kt
@@ -3,31 +3,25 @@ package edu.example.kotlindevelop.domain.orders.orderItem.dto
 import edu.example.kotlindevelop.domain.orders.orderItem.entity.OrderItem
 
 
-data class OrderItemDTO (
-
+data class OrderItemDTO(
     var productId: Long? = null, // 상품 ID
-
     var quantity: Int = 0, // 수량
-
     var price: Int = 0 // 사용자가 입력한 가격
 ) {
-
     // DTO -> 엔티티 변환 메서드
     fun toEntity(product: Product?, orders: Orders?): OrderItem {
-        return OrderItem.builder()
-            .product(product)ㅂ
-            .orders(orders)
-            .quantity(this.quantity)
-            .price(this.price)
-            .build()
+        return OrderItem(
+            product = product,
+            orders = orders,
+            quantity = quantity,
+            price = price
+        )
     }
 
     // 엔티티로부터 DTO 생성
-    constructor(orderItem: OrderItem) {
-        this.productId = orderItem.getProduct().getId()
-        this.quantity = orderItem.getQuantity()
-        this.price = orderItem.getPrice()
-    }
-
-    constructor()
+    constructor(orderItem: OrderItem) : this(
+        productId = orderItem.product?.id,
+        quantity = orderItem.quantity,
+        price = orderItem.price
+    )
 }
