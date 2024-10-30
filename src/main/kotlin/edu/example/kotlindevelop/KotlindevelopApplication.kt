220115d87b3b1b0b56d82539222f727703084c26
subbb_in <subin8350@gmail.com>
2024-10-30 14:16:41 +0900

    [fix] Rollback 에러 해결

--- src/main/kotlin/edu/example/kotlindevelop/KotlindevelopApplication.kt
+++ src/main/kotlin/edu/example/kotlindevelop/KotlindevelopApplication.kt
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
 @EnableJpaAuditing
-
 class KotlindevelopApplication
 
 fun main(args: Array<String>) {
--- src/test/kotlin/edu/example/kotlindevelop/product/repository/LossRateRepositoryTests.kt
+++ src/test/kotlin/edu/example/kotlindevelop/product/repository/LossRateRepositoryTests.kt
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.Rollback
 import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
@@ -29,6 +30,7 @@ class LossRateRepositoryTests {
 
     @Test
     @Transactional
+    @Rollback(false)
     fun 로스율등록테스트() {
         // Given - 회원 및 식재료 생성
         val member: Member = Member(
