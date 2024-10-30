dd4ddf2a0ae48f4badb0ddc88b84d5694bf44d44
asklas <asklas+jyjy0372@gmail.com>
2024-10-30 10:49:12 +0900

    feat: 중복 로그인 오류 처리 추가

--- src/main/kotlin/edu/example/kotlindevelop/member/exception/MemberException.kt
+++ src/main/kotlin/edu/example/kotlindevelop/member/exception/MemberException.kt
@@ -9,6 +9,7 @@ enum class MemberException(private val message: String, private val status: Http
     MEMBER_LOGIN_DENIED("로그인에 실패했습니다.", HttpStatus.BAD_REQUEST),
     MEMBER_NOT_REMOVED("회원정보 삭제에 실패했습니다", HttpStatus.BAD_REQUEST),
     MEMBER_IMAGE_NOT_MODIFIED("이미지 변경에 실패했습니다", HttpStatus.BAD_REQUEST),
+    MEMBER_ALREADY_EXISTS("이미 존재하는 회원입니다", HttpStatus.CONFLICT),
 
     MEMBER_ACCESSTOKEN_EXPIRED("Access 토큰이 만료되었습니다.", HttpStatus.UNAUTHORIZED),
     MEMBER_REFRESHTOKEN_EXPIRED("Refresh 토큰이 만료되었습니다.", HttpStatus.UNAUTHORIZED);
