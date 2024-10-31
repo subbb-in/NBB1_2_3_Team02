27a745ddba6be5b5e07987920d61e14c4c25b602
asklas <asklas+jyjy0372@gmail.com>
2024-10-31 15:33:29 +0900

    move: member domain 하위 디렉토리로 이동

--- src/main/kotlin/edu/example/kotlindevelop/domain/member/config/WebConfig.kt
+++ src/main/kotlin/edu/example/kotlindevelop/domain/member/config/WebConfig.kt
@@ -1,4 +1,4 @@
-package edu.example.kotlindevelop.member.config
+package edu.example.kotlindevelop.domain.member.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
